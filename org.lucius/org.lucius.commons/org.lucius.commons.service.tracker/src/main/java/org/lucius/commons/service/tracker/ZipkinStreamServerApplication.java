71c5c45aa526db675a14f4af78d2c2db71f6a82d
lucius.lv <lvxx1189@13322.com>
2017-09-15 19:07:29 +0800

    初步集成zipkin

---
+++ org.lucius/org.lucius.commons/org.lucius.commons.service.tracker/src/main/java/org/lucius/commons/service/tracker/ZipkinStreamServerApplication.java
@@ -0,0 +1,15 @@
+package org.lucius.commons.service.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+public class ZipkinStreamServerApplication {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinStreamServerApplication.class,args);
+    }
+    
+}
